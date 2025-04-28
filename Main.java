4557c38e4e8e210223973bfa9425274485546e99
Zephyros <zxphyros@gmail.com>
2025-04-28 09:58:42 -0400

    commit all the things

---
+++ Main.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        boolean currentProgramStatus;
+        final Scanner scan = new Scanner(System.in);
+
+        System.out.println("in early dev rn. things may be unpolished or buggy.\n(to exit the current sentence, type \"EXIT\".)\nnote to self. optimize this shitty code later. this is a baseplate\n");
+
+        do{
+            boolean currentSentenceGo = true;
+            System.out.println("Enter your sentence");
+            String sentence = scan.nextLine();
+
+
+            do {
+                System.out.println("\n[CURRENT SENTENCE]: " + sentence);
+                String toReplace = getInput("\nEnter the character you want to replace");
+
+                String replaceWith = getInput("\nEnter the character you will replace it with.");
+
+                if(toReplace.equals("EXIT") || replaceWith.equals("EXIT")) {currentSentenceGo = false;}
+
+                sentence = replaceChars(sentence, toReplace, replaceWith);
+
+            } while (currentSentenceGo);
+
+            currentProgramStatus = InputHelper.getYN("Do you want to do another sentence?");
+        }while (currentProgramStatus);
+
+    }
+
+    private static String getInput(String message){
+        final Scanner scan = new Scanner(System.in);
+        String input;
+        boolean go = true;
+        do {
+            System.out.println(message);
+            input = scan.nextLine();
+            if (input.length() != 1){
+                if(input.equals("EXIT")){break;}
+                System.out.println("sorry. must be one character. try again");
+            }else{go = false;}
+        }while(go);
+
+        return input;
+    }
+
+    private static String replaceChars(String sentence, String toReplace, String replaceWith){
+        final char[] sentenceDeconstructed = sentence.toCharArray();
+        for(int i = 0; i<sentenceDeconstructed.length;i++){ //use normal for loop instead of enchanced forloop here bc u need the index anyway
+            String currentChar = String.valueOf(sentenceDeconstructed[i]);
+
+            if(currentChar.equalsIgnoreCase(toReplace)){
+                sentenceDeconstructed[i] = replaceWith.charAt(0);
+            }
+        }
+        StringBuilder sentenceReconstructed = new StringBuilder();
+        for(char i:sentenceDeconstructed){sentenceReconstructed.append(i);}
+
+        return String.valueOf(sentenceReconstructed);
+    }
+}
